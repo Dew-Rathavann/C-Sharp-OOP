67eabaaf5439d0124226da5cd5703e923289116f
Rathavann <57790104+Rathavann@users.noreply.github.com>
2020-02-16 15:24:41 +0700

    Update thirt7.go

--- thirt7.go
+++ thirt7.go
@@ -2,11 +2,12 @@ package main
 import "fmt"
 func main(){
   Map := map[int]string{
-    90: "Dog",
-		91: "Cat",
-		92: "Cow",
-		93: "Bird",
-		94: "Rabbit",
+    	90: "Dog",
+	91: "Cat",
+	92: "Cow",
+	93: "Bird",
+	94: "Rabbit",
   }
-  
+	fmt.Println("Original map: ", Map)
+	value1 := Map[90]
 }
