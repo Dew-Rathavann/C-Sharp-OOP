82b9e71a96215890858695244e2a315aa8ab8889
Rathavann <57790104+Rathavann@users.noreply.github.com>
2020-02-16 13:38:15 +0700

    Update thirt4.go

--- thirt4.go
+++ thirt4.go
@@ -4,15 +4,5 @@ import (
 	"strings"
 )
 func main() {
-	var text string
-	fmt.Print("Enter text: ")
-	fmt.Scan(&text)
-	for i := 0; i < strings.Count(text); i++ {
-		if i%2 == 0 {
-			fmt.Sprint(strings.ToUpper(text[i]))
-		} else {
-			fmt.Sprint(strings.ToLower(text[i]))
-		}
-	}
-	fmt.Println(text)
+	name := "Hello! Welcome home."
 }
