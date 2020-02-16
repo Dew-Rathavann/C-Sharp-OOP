ddb0d24b675761812214f271bef0155ac7466c2a
Rathavann <57790104+Rathavann@users.noreply.github.com>
2020-02-16 16:27:43 +0700

    Update thirt9.go

--- thirt9.go
+++ thirt9.go
@@ -1,10 +1,13 @@
 package main
 import "fmt"
 type student struct{
-  firstName string
+	firstName string
 	lastName  string
 	grade     string
 	country   string
+}
+func filter(stu []student, f func(student) bool) []student{
+	
 }
 func main() {
 
