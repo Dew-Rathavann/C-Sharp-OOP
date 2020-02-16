8692dbd27ffbff81151ef37d1fd40fef30a5ef77
Rathavann <57790104+Rathavann@users.noreply.github.com>
2020-02-16 15:39:27 +0700

    Update thirt8.go

--- thirt8.go
+++ thirt8.go
@@ -3,8 +3,10 @@ import "fmt"
 func myfun(a interface{}){
   switch a.(type){
     case int:
-		  fmt.Println("Type: int, Value:", a.(int))
-    
+	fmt.Println("Type: int, Value:", a.(int))
+    case string:
+	fmt.Println("\nType: string, Value: ", a.(string))
+
   }
 }
 func main() {
