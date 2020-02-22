449d181b5426e005b8ce739e5565a277e52174ea
Rathavann Phally <57790104+Dew-Rathavann@users.noreply.github.com>
2020-02-22 23:25:01 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
+
 import (
 	"fmt"
- 	"os"
 	"io/ioutil"
-  	"strings"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
-func getDrive() (r[] string){
-  	var drive string
+
+var fileinfo os.FileInfo
+
+func getDrive() (r []string) {
+	var drive string
 	fmt.Print("Please select your drive to search(C/D): ")
 	fmt.Scan(&drive)
-  	f, err := os.Open(string(drive) + ":\\")
+	f, err := os.Open(string(drive) + ":\\")
 	if err == nil {
 		d := string(drive) + ":/"
 		r = append(r, d)
@@ -17,12 +23,12 @@ func getDrive() (r[] string){
 	}
 	return r
 }
-func FindFileFromExtension(extension []string, dir string, files *[]string){
+func FindFileFromExtension(extension []string, dir string, files *[]string) {
 	fs, err := ioutil.ReadDir(dir)
-	if err == nil{
-		for _, f := range fs{
-			for _, ex := range extension{
-				if strings.HasSuffix(f.Name(), ex){
+	if err == nil {
+		for _, f := range fs {
+			for _, ex := range extension {
+				if strings.HasSuffix(f.Name(), ex) {
 					*files = append(*files, f.Name())
 				}
 			}
@@ -33,19 +39,30 @@ func FindFileFromExtension(extension []string, dir string, files *[]string){
 		}
 	}
 }
-func CreateNewFile(){
+func CreateNewFile() {
+	drives := getDrive()
+	files := []string{}
 	newfile, err := os.Create("output.txt")
+	for _, d := range drives {
+		FindFileFromExtension([]string{".jpg"}, d, &files)
+	}
 	if err != nil {
 		return
 	}
 	defer newfile.Close()
-	newfile.WriteString("Hello")
+	for _, openfile := range files {
+		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fileinfo, err = os.Stat(files)
+		newfile.WriteString(dir)
+		newfile.WriteString(string(fileinfo.Size()))
+	}
 }
 func main() {
-	drive := getDrive()
-	files := []string{}
-	FindFileFromExtension([]string{".jpg"}, drive, &files)
-	fmt.Println(drive)
+	drives := getDrive()
+	fmt.Println(drives)
 	CreateNewFile()
 	fmt.Println("New file created successfully.")
 }
